6e76562d0a6734521b21dfea67b68c5d1f3122f3
Michael Glory M <133113993+michealglory@users.noreply.github.com>
2023-09-26 00:18:18 +0100

    handle zero division exception

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,5 +15,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             div_result = 0
         finally:
-            temp_list.append(div)
+            temp_list.append(div_result)
     return (temp_list)
