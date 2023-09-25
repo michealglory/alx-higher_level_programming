5e9c149e0cc8c5e10616e9b88fa6134c4a39cc7c
Michael Glory M <133113993+michealglory@users.noreply.github.com>
2023-09-26 00:11:33 +0100

    Handle print exceptions

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    total = 0
+    for item in range(x):
+        try:
+            print("{:d}".format(my_list[item]), end="")
+            total += 1
+        except (TypeError, ValueError, NameError):
+            pass
+    print()
+    return (total)
