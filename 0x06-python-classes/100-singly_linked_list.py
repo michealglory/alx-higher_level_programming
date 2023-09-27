9406c4fc7ff2232bceabf8936ebb0b3032911a7f
Michael Glory M <133113993+michealglory@users.noreply.github.com>
2023-09-27 05:34:11 +0100

    fix documentation issues

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -6,17 +6,20 @@ class Node:
     """"defines a node"""
 
     def __init__(self, data, next_node=None):
-
+        """Initializes a node"""
+        
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-
+        """Returns the value  of node data"""
+        
         return (self.__data)
 
     @data.setter
     def data(self, value):
+        """Sets the value of node data"""
 
         if not isinstance(value, int):
             raise TypeError('data must be an integer')
@@ -24,11 +27,13 @@ class Node:
 
     @property
     def next_node(self):
+        """Returns the value of the next node"""
 
         return (self.__next_node)
 
     @next_node.setter
     def next_node(self, value):
+        """Sets the value of the next node"""
 
         if (value is not None and not isinstance(value, Node)):
             raise TypeError('next_node must be a Node object')
@@ -40,10 +45,12 @@ class SinglyLinkedList:
     """defines a singly linked list"""
 
     def __init__(self):
+        """Constructor for the singly linked list class"""
 
         self.head = None
 
     def __str__(self):
+        """defines behavior of str on class object"""
 
         print_output = ""
         position = self.head
@@ -53,6 +60,10 @@ class SinglyLinkedList:
         return print_output[:-1]
 
     def sorted_insert(self, value):
+        """Handles Sorted insert into node
+        Args:
+            value: data to be inserted into node"""
+        
         new_node = Node(value)
         if not self.head:
             self.head = new_node
