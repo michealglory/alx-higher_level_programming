c9726ea16f9bdbf3327e89d2b3ce70e3ae7e3d16
Michael Glory M <133113993+michealglory@users.noreply.github.com>
2023-09-27 05:51:50 +0100

    add documentation

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -2,12 +2,12 @@
 import math
 """Magic class that calculates the area and circumference of a circle"""
 
+
 class MagicClass:
     """Blueprint of the margic class"""
 
     def __init__(self, radius=0):
         """Constructor of MagicClass object"""
-        
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
@@ -15,10 +15,8 @@ class MagicClass:
 
     def area(self):
         """Calculates the area of a square"""
-        
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
         """Calculates the circumference of a square"""
-        
         return 2 * math.pi * self.__radius
