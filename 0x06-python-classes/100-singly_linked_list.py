476e1d5534ed32d331e2a787c205e8155b1afb06
Michael Glory M <133113993+michealglory@users.noreply.github.com>
2023-09-27 05:04:06 +0100

    create template of a singly linked list

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""Template of a singly linked list"""
+
+
+class Node:
+    """"defines a node"""
+
+    def __init__(self, data, next_node=None):
+
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+
+        return (self.__data)
+
+    @data.setter
+    def data(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError('data must be an integer')
+        self.__data = value
+
+    @property
+    def next_node(self):
+
+        return (self.__next_node)
+
+    @next_node.setter
+    def next_node(self, value):
+
+        if (value is not None and not isinstance(value, Node)):
+            raise TypeError('next_node must be a Node object')
+
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """defines a singly linked list"""
+
+    def __init__(self):
+
+        self.head = None
+
+    def __str__(self):
+
+        print_output = ""
+        position = self.head
+        while position:
+            print_output += str(position.data) + "\n"
+            position = position.next_node
+        return print_output[:-1]
+
+    def sorted_insert(self, value):
+        
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            return
+        if value < self.head.data:
+            new_node.next_node = self.head
+            self.head = new_node
+            return
+        position = self.head
+        while position.next_node and position.next_node.data < value:
+            position = position.next_node
+        if position.next_node:
+            new_node.next_node = position.next_node
+        position.next_node = new_node
---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""Blueprint of a square"""
+
+
+class Square:
+    """define a Square."""
+
+    def __str__(self):
+        return self.pos_print()[:-1]
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple):
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if len(value) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if len([i for i in value if isinstance(i, int) and i >= 0]) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def pos_print(self):
+        square_pos = ""
+        if self.size == 0:
+            return "\n"
+        for value in range(self.position[1]):
+            square_pos += "\n"
+        for size in range(self.size):
+            for pos in range(self.position[0]):
+                square_pos += " "
+            for size in range(self.size):
+                square_pos += "#"
+            square_pos += "\n"
+        return square_pos
+
+    def my_print(self):
+        print(self.pos_print(), end="")
